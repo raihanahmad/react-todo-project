606e0a9c430b0e31a2ec699204f3e206c9f1a0f8
raihanahmad01 <dev.raihanahmad@gmail.com>
2020-07-07 13:36:15 +0600

    Updates

---
+++ precache-manifest.2d2421b46642961fad80397303f61715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f8109a60e40b0a5e0cd152283c9999",
+    "url": "/react-todo-project.git/index.html"
+  },
+  {
+    "revision": "11940f2963f1861766aa",
+    "url": "/react-todo-project.git/static/css/main.e1f9a529.chunk.css"
+  },
+  {
+    "revision": "6c74089cf19e319ddb63",
+    "url": "/react-todo-project.git/static/js/2.553a18e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-project.git/static/js/2.553a18e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11940f2963f1861766aa",
+    "url": "/react-todo-project.git/static/js/main.cad62da4.chunk.js"
+  },
+  {
+    "revision": "f641bdd8b96b7525b99f",
+    "url": "/react-todo-project.git/static/js/runtime-main.c7dae4f5.js"
+  }
+]);
